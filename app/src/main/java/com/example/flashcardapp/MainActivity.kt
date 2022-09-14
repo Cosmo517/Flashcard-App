2c71a5c70cf5f67813026e816d71572bcecea9d9
Ethan Bielecki <ebielecki2003@gmail.com>
2022-09-13 22:29:37 -0400

    Created the basics of a flashcard app. This includes only the required sections.

---
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.flashcardapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener()
+        {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+    }
+}
