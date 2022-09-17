e8650e1160b79fe7e725da639f2c09a5b0b0e661
Ethan Bielecki <ebielecki2003@gmail.com>
2022-09-17 15:43:36 -0400

    Added all the optional features as well as a 'reset answers' button.

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.flashcardapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import org.w3c.dom.Text
@@ -13,16 +15,80 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val wrongAnswer1 = findViewById<TextView>(R.id.wrong_answer_1)
+        val wrongAnswer2 = findViewById<TextView>(R.id.wrong_answer_2)
+        val correctAnswer = findViewById<TextView>(R.id.correct_answer)
+        val resetAnswers = findViewById<TextView>(R.id.reset_answers)
+        val hideAnswers = findViewById<ImageView>(R.id.toggle_choices)
+        var isShowingAnswers = true;
+
+        // Detects if the user clicks the Question to reveal the answer
         flashcardQuestion.setOnClickListener()
         {
                 flashcardQuestion.visibility = View.INVISIBLE
                 flashcardAnswer.visibility = View.VISIBLE
         }
 
+        // Detects if the user clicks the Answer to toggle back to the question
         flashcardAnswer.setOnClickListener()
         {
                 flashcardQuestion.visibility = View.VISIBLE
                 flashcardAnswer.visibility = View.INVISIBLE
         }
+
+        // Detects if wrongAnswer 1 was clicked and changes the corresponding background colors
+        wrongAnswer1.setOnClickListener()
+        {
+            wrongAnswer1.setBackgroundColor(Color.parseColor("#FF0000"))
+            correctAnswer.setBackgroundColor(Color.parseColor("#00FF00"))
+        }
+
+        // Detects if wrongAnswer 2 was clicked and changes the corresponding background colors
+        wrongAnswer2.setOnClickListener()
+        {
+            wrongAnswer2.setBackgroundColor(Color.parseColor("#FF0000"))
+            correctAnswer.setBackgroundColor(Color.parseColor("#00FF00"))
+        }
+
+        // Detects if correctAnswer was clicked and changes the corresponding background colors
+        correctAnswer.setOnClickListener()
+        {
+            correctAnswer.setBackgroundColor(Color.parseColor("#00FF00"))
+        }
+
+        // Detects if resetAnswers was clicked and resets all colors/views
+        resetAnswers.setOnClickListener()
+        {
+            wrongAnswer1.setBackgroundColor(Color.parseColor("#FFEB3B"))
+            wrongAnswer2.setBackgroundColor(Color.parseColor("#FFEB3B"))
+            correctAnswer.setBackgroundColor(Color.parseColor("#FFEB3B"))
+            hideAnswers.setImageResource(R.drawable.hide_answers)
+            isShowingAnswers = true
+            wrongAnswer1.visibility = View.VISIBLE
+            wrongAnswer2.visibility = View.VISIBLE
+            correctAnswer.visibility = View.VISIBLE
+        }
+
+        // Detects if hideAnswers was clicked and shows/hides the answers as appropriate
+        hideAnswers.setOnClickListener()
+        {
+            if (isShowingAnswers == true)
+            {
+                hideAnswers.setImageResource(R.drawable.show_answers)
+                wrongAnswer1.visibility = View.INVISIBLE
+                wrongAnswer2.visibility = View.INVISIBLE
+                correctAnswer.visibility = View.INVISIBLE
+                isShowingAnswers = false
+            }
+            else
+            {
+                hideAnswers.setImageResource(R.drawable.hide_answers)
+                wrongAnswer1.visibility = View.VISIBLE
+                wrongAnswer2.visibility = View.VISIBLE
+                correctAnswer.visibility = View.VISIBLE
+                isShowingAnswers = true
+            }
+        }
+
     }
 }
